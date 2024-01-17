8c6e8e3243943cefec8a8513dc1d125f7f3f6f06
Jilshak <mohd.jilshak@gmail.com>
2024-01-17 19:46:53 +0530

    fixed the baseUrl

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,11 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export default async function middleware(req: NextRequest) {
-  const url = new URL(req.url, "http://localhost:3000");
+  const url = new URL(req.url, process.env.BASE_URL);
   const session = req.cookies.get('next-auth.session-token')
   const publicRoutes = ['/sign-up', '/sign-in', '/forgot-pass'];
 
   if (session && publicRoutes.includes(url.pathname)) {
-    return NextResponse.redirect('http://localhost:3000/')
+    return NextResponse.redirect(`${process.env.BASE_URL}/`)
   }
 }
